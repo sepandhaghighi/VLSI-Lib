fd34e0bbd81cad08cef0f8eb8ff3b9e494a64b77
sndevil <mrezaie@Microelectronic.lab.edu>
2017-10-04 15:58:15 +0330

    NCX scripts now working for an Static CMOS NAND gate

--- lib_all/and2/tOut_A_0000r/tOut_A_0000r.sp.bat
+++
@@ -1,5 +0,0 @@
-#!/bin/sh 
-\hostname
-\date
-\cd /home/mrezaie/Downloads/ncx_inv/lib_all/and2/tOut_A_0000r
-/opt/Synopsys/HSPICE/hspice/amd64/hspice -i /home/mrezaie/Downloads/ncx_inv/lib_all/and2/tOut_A_0000r/tOut_A_0000r.sp -o /home/mrezaie/Downloads/ncx_inv/lib_all/and2/tOut_A_0000r/tOut_A_0000r > /home/mrezaie/Downloads/ncx_inv/lib_all/and2/tOut_A_0000r/tOut_A_0000r.sp.log  2>&1
--- lib_all/and2/tOut_B_0001r/tOut_B_0001r.sp.bat
+++
@@ -1,5 +0,0 @@
-#!/bin/sh 
-\hostname
-\date
-\cd /home/mrezaie/Downloads/ncx_inv/lib_all/and2/tOut_B_0001r
-/opt/Synopsys/HSPICE/hspice/amd64/hspice -i /home/mrezaie/Downloads/ncx_inv/lib_all/and2/tOut_B_0001r/tOut_B_0001r.sp -o /home/mrezaie/Downloads/ncx_inv/lib_all/and2/tOut_B_0001r/tOut_B_0001r > /home/mrezaie/Downloads/ncx_inv/lib_all/and2/tOut_B_0001r/tOut_B_0001r.sp.log  2>&1
